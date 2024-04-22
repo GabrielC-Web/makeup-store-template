44e0969464c17f20cb7dac13478cfb0c31aeb72b
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-04-22 19:23:57 -0400

    Añadidos los assets

---
+++ src/assets/images/image-routes.ts
@@ -0,0 +1,31 @@
+
+export const icons = {
+    search: 'assets/images/icons/search.svg',
+    person: 'assets/images/icons/person.svg',
+}
+
+export const logos = {
+    channel: 'assets/images/logos/channel.svg',
+    dior: 'assets/images/logos/dior.svg',
+    loreal: 'assets/images/logos/loreal.svg',
+    nars: 'assets/images/logos/nars.svg',
+    nature: 'assets/images/logos/nature.svg',
+}
+
+export const images = {
+    product_presentation: 'assets/images/products/product_presentation.png',
+    combo: 'assets/images/products/combo.png',
+    product1: 'assets/images/products/product1.png',
+    product2: 'assets/images/products/product2.png',
+    product3: 'assets/images/products/product3.png',
+    product4: 'assets/images/products/product4.png',
+    product5: 'assets/images/products/product5.png',
+    product6: 'assets/images/products/product6.png',
+    product7: 'assets/images/products/product7.png',
+    product9: 'assets/images/products/product9.png',
+    product10: 'assets/images/products/product10.png',
+    product11: 'assets/images/products/product11.png',
+    product12: 'assets/images/products/product12.png',
+    girl_makingup: 'assets/images/girl_makingup.png',
+    girl_makingup2: 'assets/images/girl_makingup2.png',
+}

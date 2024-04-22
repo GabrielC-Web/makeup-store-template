71b9791c0a21958344b382e05acdfda39bb4258b
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-04-22 19:11:55 -0400

    Creados módulos principales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export const CC_PROJECT_INITIALS = 'makeup-store-template';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'makeup-store-template';
+  title = 'meat-store-template';
+
+  ngOnInit() {
+
+    document.documentElement.style.setProperty('--v-dynamic-primary', '#E76715');
+    document.documentElement.style.setProperty('--v-dynamic-secondary', '#5B0506');
+
+  }
+
 }
+

c18e98a20ed9ff0db8f7dba99ff3af9f82b1a251
gcarreno <gcarreno@pagochinchin.com>
2024-04-29 15:42:21 -0400

    mejora vista de detalle de producto

--- src/app/modules/products/pages/products-detail-layout/products-detail-layout.component.ts
+++ src/app/modules/products/pages/products-detail-layout/products-detail-layout.component.ts
@@ -69,190 +69,139 @@ export class ProductsDetailLayoutComponent {
   /**
    * Productos del carrusel
    */
-  carouselProductsBigScreen: any[] = [
-    [
-      {
-        image: images.product5,
-        name: 'PROTECTOR SOLAR',
-        price: '158',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+  carouselProducts: any[] = [
+    {
+        "image": "assets/images/products/product5.png",
+        "name": "PROTECTOR SOLAR",
+        "price": "158",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      },
-      {
-        image: images.product6,
-        name: 'POLVO COMPACTO',
-        price: '200',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+    },
+    {
+        "image": "assets/images/products/product6.png",
+        "name": "POLVO COMPACTO",
+        "price": "200",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      },
-      {
-        image: images.product7,
-        name: 'SOMBRA 5 COLORES DIOR',
-        price: '158',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+    },
+    {
+        "image": "assets/images/products/product7.png",
+        "name": "SOMBRA 5 COLORES DIOR",
+        "price": "158",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      },
-    ],
-    [
-      {
-        image: images.product4,
-        name: 'PROTECTOR SOLAR',
-        price: '158',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+    },
+    {
+        "image": "assets/images/products/product4.png",
+        "name": "PROTECTOR SOLAR",
+        "price": "158",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      },
-      {
-        image: images.product9,
-        name: 'LABIAL MATE',
-        price: '200',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+    },
+    {
+        "image": "assets/images/products/product9.png",
+        "name": "LABIAL MATE",
+        "price": "200",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      },
-      {
-        image: images.product10,
-        name: 'LABIAL HIDRATANTE',
-        price: '200',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+    },
+    {
+        "image": "assets/images/products/product10.png",
+        "name": "LABIAL HIDRATANTE",
+        "price": "200",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      }
-    ],
-    [
-      {
-        image: images.product5,
-        name: 'PROTECTOR SOLAR',
-        price: '158',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+    },
+    {
+        "image": "assets/images/products/product5.png",
+        "name": "PROTECTOR SOLAR",
+        "price": "158",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      },
-      {
-        image: images.product6,
-        name: 'POLVO COMPACTO',
-        price: '200',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+    },
+    {
+        "image": "assets/images/products/product6.png",
+        "name": "POLVO COMPACTO",
+        "price": "200",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      },
-      {
-        image: images.product7,
-        name: 'SOMBRA 5 COLORES DIOR',
-        price: '158',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+    },
+    {
+        "image": "assets/images/products/product7.png",
+        "name": "SOMBRA 5 COLORES DIOR",
+        "price": "158",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      }
-    ],
-    [
-      {
-        image: images.product4,
-        name: 'PROTECTOR SOLAR',
-        price: '158',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+    },
+    {
+        "image": "assets/images/products/product4.png",
+        "name": "PROTECTOR SOLAR",
+        "price": "158",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      },
-      {
-        image: images.product9,
-        name: 'LABIAL MATE',
-        price: '200',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+    },
+    {
+        "image": "assets/images/products/product9.png",
+        "name": "LABIAL MATE",
+        "price": "200",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      },
-      {
-        image: images.product10,
-        name: 'LABIAL HIDRATANTE',
-        price: '200',
-        variants: [
-          '#9A8073',
-          '#B79184',
-          '#C79A85',
-          '#D7BCB3'
+    },
+    {
+        "image": "assets/images/products/product10.png",
+        "name": "LABIAL HIDRATANTE",
+        "price": "200",
+        "variants": [
+            "#9A8073",
+            "#B79184",
+            "#C79A85",
+            "#D7BCB3"
         ]
-      }
-    ]
-  ]
-  /**
-   * Productos del carrusel
-   */
-  carouselProductsSmallScreen: any[] = []
-
-  @HostListener('window:resize', ['$event'])
-  onResize(e: Event) {
-
-    //* Veo si la pantalla es lo suficientemente pequeña
-    if (window.innerWidth <= 992) {
-      this.changeListMode()
-    } else {
-      this.carouselProductsSmallScreen = []
     }
-
-  }
-
-  ngOnInit() {
-
-    this.changeListMode()
-
-  }
-
-  /**
-   * Genera una lista con todos los items del listado con secciones de items
-   */
-  changeListMode() {
-
-    //* Vacío el listado
-    this.carouselProductsSmallScreen = []
-
-    //* Creo el listado con todos los items por separado
-    this.carouselProductsBigScreen.forEach(section => {
-
-      section.forEach((item: any) => {
-
-        this.carouselProductsSmallScreen.push(item)
-
-      })
-
-    })
-
-  }
+]
 
 }

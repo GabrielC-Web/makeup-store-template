a178186dc2e933b8a8461c0c1812b3cf664fb4dc
gcarreno <gcarreno@pagochinchin.com>
2024-04-29 09:28:31 -0400

    Integrado footer de common

--- src/app/modules/main/pages/main-layout/main-layout.component.ts
+++ src/app/modules/main/pages/main-layout/main-layout.component.ts
@@ -1,5 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
+import { CmmEcomFooterConfig } from 'src/app/common/data/utils/models/ecommerce.models';
+import { icons, logos } from 'src/assets/images/image-routes';
 
 @Component({
   selector: 'pag-main-layout',
@@ -32,6 +34,52 @@ export class MainLayoutComponent {
    * Indica si debo mostrar el anuncio de arriba
    */
   showTopAnnouce: boolean = true
+
+  mainLogo: string = logos.nature
+
+  icons = icons
+
+  /**
+   * Configuración del footer
+   */
+  footerConfig: CmmEcomFooterConfig = {
+    items: [
+      {
+        icon: icons.van,
+        info: 'Envío Gratis',
+        text: 'Solo dentro la ciudad de Caracas'
+      },
+      {
+        icon: icons.return,
+        info: 'Devoluciones',
+        text: 'Devoluciones gratuitas y cambios'
+      },
+      {
+        icon: icons.purse,
+        info: 'Empaque exclusivo',
+        text: 'Empaque exclusivo por marca'
+      },
+    ],
+    socialNetworkIcons: [icons.facebook,icons.instagram],
+    businessInfo: {
+      name: 'nature',
+      direction: 'Av. Circumbalación del sol, calle 110 con av 62 de El Cafetal, Caracas',
+      phone: '+58 0424-5555607',
+      mail: 'atencion@nature.com'
+    },
+    products: [
+      'Ropa',
+      'Calzado',
+      'Accesorios',
+      'Damas',
+      'Caballeros'
+    ],
+    aboutUs: [
+      'Quienes somos',
+      'Ventas al mayor',
+      'Ofertas'
+    ]
+  }
 
   @HostListener('window:resize')
   onResize(e: Event) {

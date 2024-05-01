d7ff4f523bc771faf7393e0cf2e37dd35c766459
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-05-01 18:17:11 -0400

    Lista publicidad superior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ScrollStrategy, ScrollStrategyOptions } from '@angular/cdk/overlay';
 import { Component } from '@angular/core';
 
 export const CC_PROJECT_INITIALS = 'makeup-store-template';
@@ -10,10 +11,20 @@ export const CC_PROJECT_INITIALS = 'makeup-store-template';
 export class AppComponent {
   title = 'meat-store-template';
 
+  scrollStrategy!: ScrollStrategy;
+
+  constructor(
+    private readonly scrollStrategyOptions: ScrollStrategyOptions
+  ) {
+
+  }
+
   ngOnInit() {
 
+    this.scrollStrategy = this.scrollStrategyOptions.noop();
+
     document.documentElement.style.setProperty('--v-dynamic-primary', '#260719');
-    document.documentElement.style.setProperty('--v-dynamic-secondary', '#C90077');
+    document.documentElement.style.setProperty('--v-dynamic-secundary', '#C90077');
     document.documentElement.style.setProperty('--v-dynamic-tertiary', '#801660');
 
   }

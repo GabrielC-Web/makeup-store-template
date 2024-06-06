3fe84e50392ecba67315c2936906cbfad21bfa3d
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-06-06 18:33:44 -0400

    correcciones

--- src/app/core/components/core-brands/core-brands.component.ts
+++ src/app/core/components/core-brands/core-brands.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { logos } from 'src/assets/images/image-routes';
 
 @Component({
@@ -12,5 +13,15 @@ export class CoreBrandsComponent {
    * Logos
   */
   @Input() brandImages: string[] = []
+
+  constructor(
+    private router: Router
+  ) { }
+
+  navigate() {
+
+    this.router.navigate(['/products'])
+
+  }
 
 }
